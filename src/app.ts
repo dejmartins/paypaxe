1eeea05aa42916825abe31395de8f13db294762e
Deji Martins <dejimartins99@gmail.com>
2024-07-25 01:21:53 +0100

    test structure setting up

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,12 @@
-import express from 'express';
 import config from '../config/default';
 import connect from './shared/utils/connect';
 import log from './shared/utils/logger';
-import deserializeUser from './shared/middlewares/deserializeUser';
-import userRoutes from './modules/user/routes/userRoutes';
-import accountRoutes from './modules/account/routes/accountRoutes';
-import sessionRoutes from './modules/session/routes/sessionRoutes';
-import authRoutes from './modules/auth/routes/authRoutes';
-import passport from './modules/auth/strategy/google.strategy';
 import errorHandler from './shared/middlewares/errorHandler';
-import { generalLimiter } from './shared/middlewares/rateLimiter';
+import createServer from './shared/utils/server';
 
 const port: number = config.port;
 
-const app = express();
-
-app.use(express.json())
-app.use(deserializeUser);
-app.use(passport.initialize());
-
-app.use('/api', generalLimiter)
-app.use('/api', userRoutes)
-app.use('/api', accountRoutes)
-app.use('/api', sessionRoutes)
-app.use('/api/auth', authRoutes)
+const app = createServer();
 
 app.use(errorHandler)
 
