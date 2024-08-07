2e87f46b26b181df3c673c6bcf639d72929ce294
Deji Martins <dejimartins99@gmail.com>
2024-08-07 23:22:17 +0100

    createTransaction function

--- src/modules/transaction/service/transaction.service.ts
+++ src/modules/transaction/service/transaction.service.ts
@@ -1,5 +1,12 @@
+import { AppError } from "../../../shared/utils/customErrors";
+import TransactionModel from "../model/transaction.model";
 import { CreateTransaction } from "../types/transactionTypes";
 
 export async function createTransaction(input: CreateTransaction){
-    
+    try {
+        const transaction = await TransactionModel.create(input);
+        return transaction;
+    } catch(e: any) {
+        throw new AppError(e.message, e.statusCode);
+    }
 }
