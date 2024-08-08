8a3add6345839cd49641473efdb7b51bb639162e
Deji Martins <dejimartins99@gmail.com>
2024-08-08 04:26:29 +0100

    initiate payment - integration test

---
+++ src/__tests__/integration/payment/paymentRoutes.test.ts
@@ -0,0 +1,36 @@
+import supertest from 'supertest';
+import createServer from '../../../shared/utils/server';
+import { accountId } from '../../utils/fixtures';
+import * as PaymentService from '../../../modules/payment/service/payment.service';
+
+const app = createServer();
+
+describe('Payment', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should initiate payment and return authorization url', async () => {
+        const authorizationUrl = 'https://checkout.paystack.com/0peioxfhpn';
+        const initiatePaymentMock = jest
+            .spyOn(PaymentService, 'initiatePayment')
+            .mockResolvedValue(authorizationUrl);
+
+        const { body, statusCode } = await supertest(app)
+            .post(`/api/accounts/${accountId}/pay`)
+            .send({
+                user: 'dej@gmail.com',
+                plan: 'basic',
+                numberOfMonths: 1
+            });
+
+        expect(statusCode).toBe(200);
+        expect(body.data).toBe(authorizationUrl);
+        expect(PaymentService.initiatePayment).toHaveBeenCalledWith({
+            user: 'dej@gmail.com',
+            account: accountId,
+            plan: 'basic',
+            numberOfMonths: 1
+        });
+    });
+});
