979da92e6baff6eea358823c52563ccb7c6847b5
Deji Martins <dejimartins99@gmail.com>
2024-08-09 04:43:43 +0100

    validateAccount middleware added to goals routes

--- src/modules/financialGoal/routes/goalRoutes.ts
+++ src/modules/financialGoal/routes/goalRoutes.ts
@@ -2,12 +2,30 @@ import { Router } from "express";
 import validate from "../../../shared/middlewares/validateResource";
 import { addFinancialGoalSchema, getGoalsSchema, updateFinancialGoalSchema } from "../schema/financialGoal.schema";
 import { addGoalHandler, getGoalsHandler, updateGoalHandler } from "../controller/financialGoal.controller";
+import { validateAccountTypeAndPlan } from "../../../shared/middlewares/validateAccount";
 
 const router = Router();
 
-router.post('/accounts/:accountId/goals', validate(addFinancialGoalSchema), addGoalHandler);
-router.get('/accounts/:accountId/goals', validate(getGoalsSchema), getGoalsHandler);
-router.put('/accounts/:accountId/goals/:goalId', validate(updateFinancialGoalSchema), updateGoalHandler);
+router.post(
+    '/accounts/:accountId/goals', 
+    validateAccountTypeAndPlan(['individual'], 'basic'),
+    validate(addFinancialGoalSchema), 
+    addGoalHandler
+);
+
+router.get(
+    '/accounts/:accountId/goals',
+    validateAccountTypeAndPlan(['individual'], 'basic'), 
+    validate(getGoalsSchema), 
+    getGoalsHandler
+);
+
+router.put(
+    '/accounts/:accountId/goals/:goalId',
+    validateAccountTypeAndPlan(['individual'], 'basic'), 
+    validate(updateFinancialGoalSchema), 
+    updateGoalHandler
+);
 
 
 export default router;
