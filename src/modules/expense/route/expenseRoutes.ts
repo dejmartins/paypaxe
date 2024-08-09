294efccb772a424c885a4dbe2ce4fb7998e9dd2f
Deji Martins <dejimartins99@gmail.com>
2024-08-09 04:11:54 +0100

    added validateAccount middleware to expense routes

--- src/modules/expense/route/expenseRoutes.ts
+++ src/modules/expense/route/expenseRoutes.ts
@@ -2,11 +2,29 @@ import { Router } from "express";
 import validate from "../../../shared/middlewares/validateResource";
 import { addExpenseSchema, getRecentExpensesSchema, getTotalExpenseSchema } from "../schema/expense.schema";
 import { addExpenseHandler, getRecentExpensesHandler, getTotalExpenseHandler } from "../controller/expense.controller";
+import { validateAccountTypeAndPlan } from "../../../shared/middlewares/validateAccount";
 
 const router = Router();
 
-router.post('/accounts/:accountId/expenses', validate(addExpenseSchema), addExpenseHandler);
-router.get('/accounts/:accountId/expenses/total', validate(getTotalExpenseSchema), getTotalExpenseHandler);
-router.get('/accounts/:accountId/expenses/recent', validate(getRecentExpensesSchema), getRecentExpensesHandler);
+router.post(
+    '/accounts/:accountId/expenses', 
+    validateAccountTypeAndPlan(['individual'], 'basic'),
+    validate(addExpenseSchema), 
+    addExpenseHandler
+);
+
+router.get(
+    '/accounts/:accountId/expenses/total',
+    validateAccountTypeAndPlan(['individual'], 'basic'),
+    validate(getTotalExpenseSchema), 
+    getTotalExpenseHandler
+);
+
+router.get(
+    '/accounts/:accountId/expenses/recent',
+    validateAccountTypeAndPlan(['individual'], 'basic'),
+    validate(getRecentExpensesSchema), 
+    getRecentExpensesHandler
+);
 
 export default router;
