8ddc719370585ce28554072aad686d90f824a10c
Deji Martins <dejimartins99@gmail.com>
2024-11-27 11:10:10 +0100

    deleted savings log route

--- src/shared/utils/server.ts
+++ src/shared/utils/server.ts
@@ -9,7 +9,6 @@ import goalRoutes from '../../modules/financialGoal/routes/goalRoutes';
 import authRoutes from '../../modules/auth/routes/authRoutes';
 import paymentRoutes from '../../modules/payment/routes/paymentRoutes';
 import ocrLogRoutes from '../../modules/utility/routes/ocrLogRoutes';
-import logSavingsRoutes from '../../modules/financialGoal/routes/logSavingsRoutes';
 import passport from '../../modules/auth/strategy/google.strategy'; 
 import cors from 'cors';
 import { generalLimiter } from '../middlewares/rateLimiter';
@@ -36,7 +35,6 @@ function createServer() {
     app.use('/api', goalRoutes)
     app.use('/api', paymentRoutes)
     app.use('/api', ocrLogRoutes)
-    app.use('/api', logSavingsRoutes)
     app.use('/api/auth', authRoutes)
 
     return app;
