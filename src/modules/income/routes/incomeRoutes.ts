b15f5c2cc3ee13eec84f040da97b262938dd1680
Deji Martins <dejimartins99@gmail.com>
2024-12-15 17:21:44 +0100

    removed checks

--- src/modules/income/routes/incomeRoutes.ts
+++ src/modules/income/routes/incomeRoutes.ts
@@ -24,40 +24,40 @@ router.get(
 
 router.get(
     '/accounts/:accountId/incomes/recent',
-    validateSubscription,
-    validateAccountTypeAndPlan(['individual'], 'basic'), 
+    // validateSubscription,
+    // validateAccountTypeAndPlan(['individual'], 'basic'), 
     validate(getRecentIncomesSchema), 
     getRecentIncomesHandler
 );
 
 router.patch(
     '/accounts/:accountId/incomes/:incomeId/soft-delete',
-    validateSubscription,
-    validateAccountTypeAndPlan(['individual'], 'basic'),
+    // validateSubscription,
+    // validateAccountTypeAndPlan(['individual'], 'basic'),
     validate(softDeleteIncomeSchema), 
     softDeleteIncomeHandler
 );
 
 router.get(
     '/accounts/:accountId/incomes/deleted',
-    validateSubscription,
-    validateAccountTypeAndPlan(['individual'], 'basic'),
+    // validateSubscription,
+    // validateAccountTypeAndPlan(['individual'], 'basic'),
     validate(getDeletedIncomesSchema),
     getDeletedIncomesHandler
 );
 
 router.patch(
     '/accounts/:accountId/incomes/:incomeId',
-    validateSubscription,
-    validateAccountTypeAndPlan(['individual'], 'basic'),
+    // validateSubscription,
+    // validateAccountTypeAndPlan(['individual'], 'basic'),
     validate(updateIncomeSchema), 
     updateIncomeHandler
 );
 
 router.get(
     '/accounts/:accountId/incomes/export',
-    validateSubscription,
-    validateAccountTypeAndPlan(['individual'], 'basic'),
+    // validateSubscription,
+    // validateAccountTypeAndPlan(['individual'], 'basic'),
     validate(exportIncomeSchema),
     exportIncomeHandler
 );
