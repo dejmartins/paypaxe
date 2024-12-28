a52954612938e05cdedf8a19d72dddab06fe9dcf
Deji Martins <dejimartins99@gmail.com>
2024-12-28 12:42:43 +0100

    added id when creating session

--- src/modules/session/controller/session.controller.ts
+++ src/modules/session/controller/session.controller.ts
@@ -14,7 +14,7 @@ export const createUserSessionHandler =  asyncHandler(async (req: Request, res: 
 
     const { accessToken, refreshToken } = await generateSessionTokens(user, session.session._id);
 
-    return res.send(successResponse({ accessToken, refreshToken, accounts: session.accounts, user: session.user }, "Session created successfully"))
+    return res.send(successResponse({ accessToken, refreshToken, accounts: session.accounts, user: {...session.user, id: user.id} }, "Session created successfully"))
 })
 
 export const getUserSessionsHandler = asyncHandler(async (req: Request, res: Response) => {
