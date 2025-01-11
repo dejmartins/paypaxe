72bad10244c33c52123d4b031c06dab597384e57
Deji Martins <dejimartins99@gmail.com>
2025-01-11 08:54:22 -0500

    added expense checks

--- src/modules/expense/route/expenseRoutes.ts
+++ src/modules/expense/route/expenseRoutes.ts
@@ -8,62 +8,64 @@ const router = Router();
 
 router.post(
     '/accounts/:accountId/expenses',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(addExpenseSchema), 
     addExpenseHandler
 );
 
 router.get(
     '/accounts/:accountId/expenses/total',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(getTotalExpenseSchema), 
     getTotalExpenseHandler
 );
 
 router.get(
     '/accounts/:accountId/expenses/recent',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(getRecentExpensesSchema), 
     getRecentExpensesHandler
 );
 
 router.patch(
     '/accounts/:accountId/expenses/:expenseId/soft-delete',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(softDeleteExpenseSchema), 
     softDeleteExpenseHandler
 );
 
 router.get(
     '/accounts/:accountId/expenses/deleted',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(getDeletedExpensesSchema), 
     getDeletedExpensesHandler
 );
 
 router.patch(
     '/accounts/:accountId/expenses/:expenseId',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(updateExpenseSchema), 
     updateExpenseHandler
 );
 
 router.get(
     '/accounts/:accountId/expenses/export',
-    // validateSubscription,
-    // validateAccountTypeAndPlan(['individual', 'family'], 'basic'),
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(exportExpenseSchema),
     exportExpenseHandler
 );
 
 router.get(
     "/accounts/:accountId/expense-breakdown",
+    validateSubscription,
+    validateAccountTypeAndPlan(['individual', 'family'], ['basic', 'premium']),
     validate(expenseBreakdownSchema),
     getExpenseBreakdownHandler
 );
